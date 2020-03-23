fad638da8998a8d7ae33c2d92a1738fe8eeccbcb
dpozinen <dpozinen@gmail.com>
2020-03-23 15:03:16 +0200

    add more Order states

--- src/main/java/dpozinen/manager/model/order/OrderState.java
+++ src/main/java/dpozinen/manager/model/order/OrderState.java
@@ -4,5 +4,19 @@ package dpozinen.manager.model.order;
  * @author dpozinen
  */
 public enum OrderState {
-	DELAYED, DONE, QUEUED
+	DELAYED, DONE, QUEUED,
+	IN_PROGRESS {
+		@Override
+		public String toString() {
+			return "IN PROGRESS";
+		}
+	},
+
+	PAYED,
+	NOT_PAYED {
+		@Override
+		public String toString() {
+			return "NOT PAYED";
+		}
+	}
 }
